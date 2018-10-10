d2933fd39067d6cc74e4436870486c69b052b694
ShoGinn <2660603+ShoGinn@users.noreply.github.com>
2018-10-10 16:29:51 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
+
 SOURCE=/config/source
 
 mkdir -p $SOURCE
 
 for FILENAME in VirtualRadar.tar.gz VirtualRadar.WebAdminPlugin.tar.gz VirtualRadar.exe.config.tar.gz VirtualRadar.DatabaseWriterPlugin.tar.gz VirtualRadar.CustomContentPlugin.tar.gz
-do
-if ! [ -f $SOURCE/$FILENAME ]; then
-	curl -o $SOURCE/$FILENAME http://www.virtualradarserver.co.uk/Files/$FILENAME
-else
-    curl -o $SOURCE/$FILENAME -z $SOURCE/$FILENAME http://www.virtualradarserver.co.uk/Files/$FILENAME
-fi
-tar -xf $SOURCE/$FILENAME -C /opt/vrs/
-done
-if ! [ -f $SOURCE/admin_created ]; then
+	do
+	if [[ ! -f $SOURCE/$FILENAME ]]; then
+		echo "Downloading File: $FILENAME"
+		curl -o $SOURCE/$FILENAME http://www.virtualradarserver.co.uk/Files/$FILENAME --silent
+	else
+		echo "File Exists, checking for newer version: $FILENAME"
+		curl -o $SOURCE/$FILENAME -z $SOURCE/$FILENAME http://www.virtualradarserver.co.uk/Files/$FILENAME --silent
+	fi
+
+	tar -xf $SOURCE/$FILENAME -C /opt/vrs/
+
+	done
+
+if [[ ! -f $SOURCE/admin_created ]]; then
+	echo "First Time Run: Admin account created"
+	echo "Username: admin"
+	echo "Password: password"
 	touch $SOURCE/admin_created
 	mono /opt/vrs/VirtualRadar.exe -createAdmin:admin -password:password -nogui
 else	
